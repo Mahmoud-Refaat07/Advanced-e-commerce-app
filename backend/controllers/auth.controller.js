20381e3abb76e902f25c2f43e919cea4331272b5
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-12-02 00:18:19 +0200

    implemented login endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { storeRefreshToken, deleteRefreshToken } from "../lib/redis.js";
 import { generateTokens } from "../lib/generateTokens.js";
@@ -28,10 +29,40 @@ export const signup = async (req, res) => {
 
     res.status(200).json({ message: "User Created Sucessfully", user });
   } catch (error) {
+    console.log("Error in signup endpoint");
     res.status(500).json({ message: error.message });
   }
 };
-export const login = async (req, res) => {};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User Not Found" });
+
+    // compare password from database
+    if (!(await user.comparePassword(password))) {
+      return res.status(400).json({ message: "Incorrect Password" });
+    }
+
+    // Authentication
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+    setCookies(res, accessToken, refreshToken);
+
+    res.status(200).json({
+      id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      cartItems: user.cartItems,
+    });
+  } catch (error) {
+    console.log("Error in login endpoint");
+    res.status(500).json({ message: error.message });
+  }
+};
+
 export const logout = async (req, res) => {
   try {
     const refreshToken = req.cookies.refreshToken;
@@ -49,6 +80,7 @@ export const logout = async (req, res) => {
       res.json({ message: "Logout Successfully" });
     }
   } catch (error) {
+    console.log("Error in logout endpoint");
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
