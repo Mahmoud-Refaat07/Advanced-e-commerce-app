2efe6a3f7a616178cd54cb97361500760d16fa58
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-12-07 21:14:19 +0200

    implemented signup page

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,54 @@
+import { Mail, User, Lock } from "lucide-react";
+
+interface Props {
+  label: string;
+  id: string;
+  type: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder: string;
+  icon: string;
+}
+
+const Input = ({
+  label,
+  id,
+  type,
+  value,
+  onChange,
+  placeholder,
+  icon,
+}: Props) => {
+  return (
+    <div>
+      <label htmlFor={id} className="block text-sm font-medium text-gray-300">
+        {label}
+      </label>
+      <div className="mt-1 relative rounded-md shadow-sm">
+        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          {icon === "user" ? (
+            <User className="h-5 w-5 text-gray-400" />
+          ) : icon === "email" ? (
+            <Mail className="h-5 w-5 text-gray-400" />
+          ) : icon === "password" ? (
+            <Lock className="h-5 w-5 text-gray-400" />
+          ) : (
+            ""
+          )}
+        </div>
+        <input
+          id={id}
+          type={type}
+          value={value}
+          onChange={onChange}
+          className="block w-full px-3 py-2 pl-10 bg-gray-700 border border-gray-600 rounded-md shadow-sm
+          placeholder-gray-400 focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 sm:text-sm"
+          placeholder={placeholder}
+          required
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,5 +1,123 @@
+import { UserPlus, ArrowRight, Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import Input from "../components/Input";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const loading = false;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="w-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <motion.div
+        className="sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <h2 className="mt-6 text-3xl text-emerald-400 font-extrabold text-center ">
+          Create your account
+        </h2>
+      </motion.div>
+
+      <motion.div
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.2, delay: 0.3 }}
+      >
+        <div className="bg-gray-800 py-8 px-4 shadow sm:rounded-lg sm:px-10 ">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <Input
+              label="Fullname"
+              id="name"
+              type="text"
+              value={formData.name}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+              placeholder="John Doe"
+              icon="user"
+            />
+            <Input
+              label="Email"
+              id="email"
+              type="email"
+              value={formData.email}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              placeholder="Johndoe@gmail.com"
+              icon="email"
+            />
+            <Input
+              label="Password"
+              id="password"
+              type="password"
+              value={formData.password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              placeholder="*******"
+              icon="password"
+            />
+            <Input
+              label="Confirm Password"
+              id="confirmPassword"
+              type="password"
+              value={formData.confirmPassword}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setFormData({ ...formData, confirmPassword: e.target.value })
+              }
+              placeholder="*******"
+              icon="password"
+            />
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent 
+							rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600
+							 hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2
+							  focus:ring-emerald-500 transition duration-150 ease-in-out disabled:opacity-50 "
+              disabled={loading}
+            >
+              {loading ? (
+                <Loader
+                  className="mr-2 h-5 w-5 animate-spin"
+                  aria-hidden="true"
+                >
+                  Loading...
+                </Loader>
+              ) : (
+                <>
+                  <UserPlus className="mr-2 h-5 w-5" aria-hidden="true" />
+                  Sign Up
+                </>
+              )}
+            </button>
+          </form>
+          <p className="mt-8 text-center text-sm text-gray-300">
+            Already have an account?{" "}
+            <Link
+              to="login"
+              className="text-meduim text-emerald-400 hover:text-emerald-300"
+            >
+              Login Here <ArrowRight className="inline h-4 w-4" />
+            </Link>
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default SignUpPage;
