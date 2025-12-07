2adab3e54c9cadb5f0fed19135be5c7d80a3a0f7
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-12-07 23:18:20 +0200

    refactoring

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,12 @@ dotenv.config({ path: "../.env" });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors(), {
-  origin: "http://localhost:5173",
-  credentials: true,
-});
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
