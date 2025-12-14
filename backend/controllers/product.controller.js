7f380c8459264bc26f4617d5a09d2fb128221d9c
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-12-14 19:20:23 +0200

    typo fix

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -4,8 +4,8 @@ import { redis } from "../lib/redis.js";
 
 export const getAllProducts = async (req, res) => {
   try {
-    const product = await Product.find({});
-    res.status(200).json({ product });
+    const products = await Product.find({});
+    res.status(200).json({ products });
   } catch (error) {
     console.log("Error in getAllProducts endpoint " + error.message);
     res.status(500).json({ message: "Server Error", error: error.message });
