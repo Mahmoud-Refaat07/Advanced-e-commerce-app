1a24c844efc46f7c7061770192d8ff1dd0d198f6
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-21 13:22:19 +0200

    prevent strict sameSite function

--- backend/lib/cookies.js
+++ backend/lib/cookies.js
@@ -9,7 +9,7 @@ export const setCookies = (res, accessToken, refreshToken) => {
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict", // prevent CSRF attacks, cross-site request frogery attack
+    sameSite: "none", // prevent CSRF attacks, cross-site request frogery attack
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7days
   });
 };
