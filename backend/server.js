93493dfb89571adaf9a94d4500a02eed9d4302f3
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-21 02:02:02 +0200

    modified server deployments

--- backend/server.js
+++ backend/server.js
@@ -35,16 +35,13 @@ app.use("/api/coupon", couponRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-  // Fallback for React Router (Express 5 safe)
-  app.all("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 console.log("About to start server...");
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
   connectDatabase();
 });
