3c5ccfb6c8311f544a99c02643d0050d9b614751
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-21 13:50:14 +0200

    modified client url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://advanced-e-commerce-app-production.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
   withCredentials: true,
 });
 
