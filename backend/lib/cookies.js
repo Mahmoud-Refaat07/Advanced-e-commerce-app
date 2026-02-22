f3e4deca79f9461f731a66c90a266d3445573e87
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2026-02-22 12:50:08 +0200

    modified cookies cross-site request

--- backend/lib/cookies.js
+++ backend/lib/cookies.js
@@ -2,14 +2,14 @@ export const setCookies = (res, accessToken, refreshToken) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict", // prevent CSRF attacks, cross-site request frogery attack
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // prevent CSRF attacks, cross-site request frogery attack
     maxAge: 15 * 60 * 1000, // 15min
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none", // prevent CSRF attacks, cross-site request frogery attack
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // prevent CSRF attacks, cross-site request frogery attack
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7days
   });
 };
